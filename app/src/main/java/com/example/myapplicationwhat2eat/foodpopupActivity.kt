fa6edcaa0173c4dd23d741713c97055bf0aec8a4
Amel <amel.93@hotmail.com>
2021-02-15 14:18:57 +0100

    aa

---
+++ app/src/main/java/com/example/myapplicationwhat2eat/foodpopupActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplicationwhat2eat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class foodpopupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.foodpopup)
+    }
+}
